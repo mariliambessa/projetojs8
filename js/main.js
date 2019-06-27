9a34838b9a56352bbd7c6268695fde3a2bb34efe
Marilia Bessa <mariliambessa@gmail.com>
2019-06-26 21:10:41 -0300

    playing with JQuery

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+	$('button').click(function(){
+		$('.navbar-brand').html(`
+			<img src="${dados.avatar_url}" class="avatar d-inline-block align-top">
+			${dados.name}
+		`);
+
+		$('.navbar-brand').addClass('d-flex align-items-center');
+		$('nav').toggleClass('trocar-cor');
+		$('nav').removeClass('bg-danger');
+	});
+
+});
